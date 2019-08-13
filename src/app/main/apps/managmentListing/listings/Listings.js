960ae8f04783188974f60168041c9f602923bf68
Lucas <csalucasnascimento@gmail.com>
2019-08-13 10:07:19 +1000

    cleaning errors

--- src/app/main/apps/managmentListing/listings/Listings.js
+++ src/app/main/apps/managmentListing/listings/Listings.js
@@ -1,29 +1,21 @@
-import React from 'react';
-import { SpacenowPageCarded } from '@spacenow';
-import withReducer from 'app/store/withReducer';
-import ListingsTable from './ListingsTable';
-import UsersHeader from './UsersHeader';
-//import reducer from './store/reducers';
+import React from "react";
+import { SpacenowPageCarded } from "@spacenow";
+import withReducer from "app/store/withReducer";
+import ListingsTable from "./ListingsTable";
+import ListingsHeader from "./ListingsHeader";
 
 function Listings() {
-    return (
-        <SpacenowPageCarded
-            classes={{
-                content: "flex",
-                header: "min-h-72 h-72 sm:h-136 sm:min-h-136"
-            }}
-            header={
-                <UsersHeader />
-            }
-            content={
-                <ListingsTable />
-            }
-            innerScroll
-        />
-        // <>
-        // listings
-        // </>
-    );
+  return (
+    <SpacenowPageCarded
+      classes={{
+        content: "flex",
+        header: "min-h-72 h-72 sm:h-136 sm:min-h-136"
+      }}
+      header={<ListingsHeader />}
+      content={<ListingsTable />}
+      innerScroll
+    />
+  );
 }
 
-export default withReducer('managmentListing')(Listings);
+export default withReducer("managmentListing")(Listings);
