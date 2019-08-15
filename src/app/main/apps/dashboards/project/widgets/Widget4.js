da3e52cfaa990fa6345051dd24925145e756f064
rafael <rafanascimento.au@gmail.com>
2019-08-15 11:40:04 +1000

    just took of some imported component

--- src/app/main/apps/dashboards/project/widgets/Widget4.js
+++ src/app/main/apps/dashboards/project/widgets/Widget4.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Icon, Typography, Paper, IconButton } from "@material-ui/core";
 
-import { useDispatch, useSelector } from "react-redux";
+//import { useDispatch, useSelector } from "react-redux";
 
 import * as Actions from "./../store/actions";
 
