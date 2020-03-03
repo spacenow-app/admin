9af216ee7e7968bb83e068701561cbe09c08ec72
Camila <kmi_218@hotmail.com>
2020-03-03 16:00:10 +1100

    add accept booking function admin area

--- src/app/App.js
+++ src/app/App.js
@@ -1,46 +1,49 @@
-import React from 'react';
-import { SpacenowAuthorization, SpacenowLayout, SpacenowTheme } from '@spacenow';
-import Provider from 'react-redux/es/components/Provider';
-import { Router } from 'react-router-dom';
-import jssExtend from 'jss-extend';
-import history from '@history';
-import { Auth } from './auth';
-import store from './store';
-import AppContext from './AppContext';
-import routes from './spacenow-configs/routesConfig';
-import { create } from 'jss';
-import { StylesProvider, jssPreset, createGenerateClassName } from '@material-ui/styles';
+import React from 'react'
+import { SpacenowAuthorization, SpacenowLayout, SpacenowTheme } from '@spacenow'
+import Provider from 'react-redux/es/components/Provider'
+import { Router } from 'react-router-dom'
+import jssExtend from 'jss-extend'
+import history from '@history'
+import { Auth } from './auth'
+import store from './store'
+import AppContext from './AppContext'
+import routes from './spacenow-configs/routesConfig'
+import { create } from 'jss'
+import { StylesProvider, jssPreset, createGenerateClassName } from '@material-ui/styles'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const jss = create({
-    ...jssPreset(),
-    plugins: [...jssPreset().plugins, jssExtend()],
-    insertionPoint: document.getElementById('jss-insertion-point'),
-});
+  ...jssPreset(),
+  plugins: [...jssPreset().plugins, jssExtend()],
+  insertionPoint: document.getElementById('jss-insertion-point')
+})
 
-const generateClassName = createGenerateClassName();
+const generateClassName = createGenerateClassName()
 
 const App = () => {
-    return (
-        <AppContext.Provider
-            value={{
-                routes
-            }}
-        >
-            <StylesProvider jss={jss} generateClassName={generateClassName}>
-                <Provider store={store}>
-                    <Auth>
-                        <Router history={history}>
-                            <SpacenowAuthorization>
-                                <SpacenowTheme>
-                                    <SpacenowLayout />
-                                </SpacenowTheme>
-                            </SpacenowAuthorization>
-                        </Router>
-                    </Auth>
-                </Provider>
-            </StylesProvider>
-        </AppContext.Provider>
-    );
-};
+  return (
+    <AppContext.Provider
+      value={{
+        routes
+      }}
+    >
+      <StylesProvider jss={jss} generateClassName={generateClassName}>
+        <Provider store={store}>
+          <Auth>
+            <Router history={history}>
+              <SpacenowAuthorization>
+                <SpacenowTheme>
+                  <ToastContainer hideProgressBar />
+                  <SpacenowLayout />
+                </SpacenowTheme>
+              </SpacenowAuthorization>
+            </Router>
+          </Auth>
+        </Provider>
+      </StylesProvider>
+    </AppContext.Provider>
+  )
+}
 
-export default App;
+export default App
