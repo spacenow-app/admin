d2c9a6672a109d96147867874d4b255a3162a35a
Lucas Nascimento <csalucasnascimento@gmail.com>
2020-03-13 15:17:51 +1100

    correcting name

--- src/app/main/apps/dashboards/project/widgets/WidgetListings.js
+++ src/app/main/apps/dashboards/project/widgets/WidgetListings.js
@@ -61,14 +61,11 @@ function WidgetListings(props) {
                         </MenuItem>
                         {
                             props.categories && props.categories.data.map((category) => (
-                                category.subCategories.map((item) => (
-                                    item.subCategory && <MenuItem key={item.id} onClick={() => _setCategoryDay(item.id, currentCategoryDay.days)}>
-                                        <ListItemText primary={`${category.itemName} -> ${item.subCategory.itemName}`} />
-                                    </MenuItem> ||
-                                    <MenuItem key={category.id} onClick={() => _setCategoryDay(category.id, currentCategoryDay.days)}>
-                                        <ListItemText primary={`${category.itemName}`} />
+                                category.subCategories.map((item) =>
+                                    <MenuItem key={item.id} onClick={() => _setCategoryDay(item.id, currentCategoryDay.days)}>
+                                        <ListItemText primary={`${category.itemName} -> ${item.subCategory && item.subCategory.itemName}`} />
                                     </MenuItem>
-                                ))
+                                )
                             ))
                         }
                     </MenuList>
